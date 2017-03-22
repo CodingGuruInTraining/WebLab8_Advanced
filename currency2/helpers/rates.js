427ed9b9c406ca79fdf8b3e52feddbec443fc6b5
mf <myemail@isawesome.com>
2017-03-22 12:25:28 -0500

    autoindents

--- currency2/helpers/rates.js
+++ currency2/helpers/rates.js
@@ -4,29 +4,29 @@ var moment = require('moment');
 var baseURL = 'https://openexchangerates.org/api/latest.json';
 
 function ratesRequest(callback) {
-  var APIKEY = process.env.RATES_API_KEY;
-  var queryParam = { 'app_id' : APIKEY };
-  request( {uri: baseURL, qs: queryParam}, function(error, rates_response, body) {
-    // todo deal with errors
+    var APIKEY = process.env.RATES_API_KEY;
+    var queryParam = { 'app_id' : APIKEY };
+    request( {uri: baseURL, qs: queryParam}, function(error, rates_response, body) {
+        // todo deal with errors
 
-    if (!error && rates_response.statusCode == 200) {
-      console.log("no errors?");
+        if (!error && rates_response.statusCode == 200) {
+            console.log("no errors?");
 
-      try {
-        var ratesJSON = JSON.parse(body);
-      } catch (error) {
-        return callback(error);   // just for cactching JSON parsing errors.
-      }
+            try {
+                var ratesJSON = JSON.parse(body);
+            } catch (error) {
+                return callback(error);   // just for cactching JSON parsing errors.
+            }
 
-      return callback(null, ratesJSON.rates);
+            return callback(null, ratesJSON.rates);
 
-    }
+        }
 
-    else {
-      return callback(error);
-      // todo what if no error and status is not 200?
-    }
-  });
+        else {
+            return callback(error);
+            // todo what if no error and status is not 200?
+        }
+    });
 }
 
 
